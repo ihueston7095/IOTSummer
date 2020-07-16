61ed9267bc5a2d7fa0dd5534755e22b23a705b4e
ihueston7095 <68338339+ihueston7095@users.noreply.github.com>
2020-07-16 14:25:07 -0400

    Create RGB.ino

---
+++ RGB/RGB.ino
@@ -0,0 +1,22 @@
+int red = 4;
+int green = 5;
+int blue = 2;
+
+void setup() {
+  pinMode(red, OUTPUT);
+  pinMode(green, OUTPUT);
+  pinMode(blue, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(red, LOW);
+  digitalWrite(green, HIGH);
+  digitalWrite(blue, HIGH);
+  delay(500);
+  digitalWrite(red, HIGH);
+  digitalWrite(green, LOW);
+  delay(500);
+  digitalWrite(green, HIGH);
+  digitalWrite(blue, LOW);
+  delay(500);
+}
